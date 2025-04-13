e8e901a1a22d6c621800f9857f0b400c469fd113
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-04-13 23:17:47 +0200

    Add about page

--- chapter-3-personal-website/pages/views.py
+++ chapter-3-personal-website/pages/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page_view(request):
     return HttpResponse('<h1>Homepage</h1>')
+
+def about_page_view(request):
+    return render(request, 'pages/about.html')
