0e6b4fdb76b9e02c2c02a40fb703b073194da5e9
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-05-23 00:12:59 +0200

    Add further tests (testing url names)

--- chapter-4-company-website/pages/tests.py
+++ chapter-4-company-website/pages/tests.py
@@ -1,4 +1,5 @@
 from django.test import SimpleTestCase
+from django.urls import reverse
 
 
 class HomePageTests(SimpleTestCase):
@@ -6,7 +7,15 @@ class HomePageTests(SimpleTestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
 class AboutPageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse('about'))
         self.assertEqual(response.status_code, 200)
