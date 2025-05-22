e3c7c804d4107d9faee69606bf5af39b44546d76
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-05-23 00:22:36 +0200

    Addd further tests, check the template name and content

--- chapter-4-company-website/pages/tests.py
+++ chapter-4-company-website/pages/tests.py
@@ -11,6 +11,15 @@ class HomePageTests(SimpleTestCase):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
+    def test_template_name_correct(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_template_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, '<h1>Company Homepage</h1>')
+
+
 class AboutPageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get('/about/')
@@ -19,3 +28,11 @@ class AboutPageTests(SimpleTestCase):
     def test_url_available_by_name(self):
         response = self.client.get(reverse('about'))
         self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse('about'))
+        self.assertTemplateUsed(response, 'about.html')
+
+    def test_template_content(self):
+        response = self.client.get(reverse('about'))
+        self.assertContains(response, '<h1>Company About Page</h1>')
