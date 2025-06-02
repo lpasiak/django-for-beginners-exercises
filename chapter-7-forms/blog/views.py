a9ddaac3ed6da8b7262f15e1163aa0f9e147fd79
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-06-02 23:37:10 +0200

    Change function-based views to generic class-based views

--- chapter-7-forms/blog/views.py
+++ chapter-7-forms/blog/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Post
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'post_detail.html', {'post': post})
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'posts'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
