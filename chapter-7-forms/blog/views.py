c1aba7f8c8435a0866e15cef281ae517f6a676c3
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-06-03 21:39:23 +0200

    Add DeleteView

--- chapter-7-forms/blog/views.py
+++ chapter-7-forms/blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
 
@@ -22,8 +22,16 @@ class BlogCreateView(CreateView):
     fields = ['title', 'author', 'body']
     success_url = ''
 
+
 class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
     success_url = reverse_lazy('home')
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+    context_object_name = 'post'
