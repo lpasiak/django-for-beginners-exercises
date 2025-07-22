cc03969004a4fb1de36e754c255d0455aebac06e
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-07-22 23:50:39 +0200

    Fixed faulty '<pk:int>'

--- chapter-13-articles-app/articles/urls.py
+++ chapter-13-articles-app/articles/urls.py
@@ -6,7 +6,7 @@ from .views import (ArticleListView,
 
 urlpatterns = [
    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
-   path('<pk:int>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
-   path('<pk:int>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+   path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+   path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
    path('', ArticleListView.as_view(), name='article_list')
 ]
