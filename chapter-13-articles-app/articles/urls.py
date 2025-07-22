0dfb2f25383bdf8b4ac21580738a0778db9544f5
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-07-22 23:48:22 +0200

    Removed old files. Added urls and views in articles.

--- chapter-13-articles-app/articles/urls.py
+++ chapter-13-articles-app/articles/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import (ArticleListView,
+                    ArticleDetailView,
+                    ArticleUpdateView,
+                    ArticleDeleteView)
 
 urlpatterns = [
+   path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+   path('<pk:int>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+   path('<pk:int>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
    path('', ArticleListView.as_view(), name='article_list')
 ]
