25354b2731dfa3ac156d91e4487e35dfe987af12
Lukasz Pasiak <lukasz.pasiak@icloud.com>
2025-07-25 02:02:01 +0200

    Added comment possibility

--- chapter-15-comments/articles/admin.py
+++ chapter-15-comments/articles/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
 
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+@admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+    ]
     list_display = [
         'title',
         'author',
         'date'
     ]
 
-
-admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
